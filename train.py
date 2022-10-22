6bc6ea27bb79710b4e9a3f571a096eff5438053e
kyunghunNam <61224739+Namkyunghun@users.noreply.github.com>
2022-10-22 11:57:05 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,163 +1,140 @@
 import argparse
+import os
 import torch
-
-from model.smooth_cross_entropy import smooth_crossentropy,trades_loss
-from utils.cifar import Cifar,Cifar100
-from utils.log import Log
-from utils.initialize import initialize
-from utils.step_lr import StepLR
-from utils.Esam import ESAM
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, CIFAR100, ImageFolder
+from timm.loss import LabelSmoothingCrossEntropy
+from homura.vision.models.cifar_resnet import wrn28_2, wrn28_10, resnet20, resnet56, resnext29_32x4d, cifar_resnet18
+from homura.vision.models.PyramidNet import PyramidNet
+from asam import ASAM, SAM
 from torch.utils.tensorboard import SummaryWriter
-import os 
-from utils.mail import send_email
 
-from utils.options import args,setup_model
-from utils.MiscTools import count_parameters
-from utils.dist_util import get_world_size
+def load_cifar(data_loader, batch_size=256, num_workers=2):
+    if data_loader == CIFAR10:
+        mean = (0.4914, 0.4822, 0.4465)
+        std = (0.2023, 0.1994, 0.2010)
+    if data_loader == CIFAR100:
+        mean = (0.5071, 0.4867, 0.4408)
+        std = (0.2675, 0.2565, 0.2761)
 
-import torch.nn.functional as F
-import logging
-from datetime import timedelta
-from apex import amp
-from torch.nn.parallel import DistributedDataParallel as DDP
+    # Transforms
+    train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
+                         transforms.RandomHorizontalFlip(),
+                         transforms.ToTensor(),
+                         transforms.Normalize(mean, std)])
 
-logger = logging.getLogger(__name__)
-def train(args,model):
+    test_transform = transforms.Compose([
+                         transforms.ToTensor(),
+                         transforms.Normalize(mean, std)
+                         ])
+    
+    # DataLoader
+    train_set = data_loader(root='./data', train=True, download=True, transform=train_transform)
+    test_set = data_loader(root='./data', train=False, download=True, transform=test_transform)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, 
+            num_workers=num_workers)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, 
+            num_workers=num_workers)
+    return train_loader, test_loader
+   
 
-    initialize(args, seed=42)
-    device = args.device
+def train(args): 
+    # Data Loader
+    train_loader, test_loader = load_cifar(eval(args.dataset), args.batch_size)
+    num_classes = 10 if args.dataset == 'CIFAR10' else 100
 
-    dataset = Cifar(args) if args.dataset =="cifar10" else Cifar100(args)
-    log = Log(log_each=10)
+    # Model
+    model = eval(args.model)(num_classes=num_classes).cuda()
 
-    if args.SCE_loss =="True":
-        loss_fct = smooth_crossentropy
+    # Minimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, 
+                                momentum=args.momentum, weight_decay=args.weight_decay)
+    minimizer = eval(args.minimizer)(optimizer, model, rho=args.rho, eta=args.eta)
+    
+    # Learning Rate Scheduler
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(minimizer.optimizer, args.epochs)
+    
+    writer = SummaryWriter(log_dir=os.path.join("logs"))
+
+    # Loss Functions
+    if args.smoothing:
+        criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing)
     else:
-        loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
+        criterion = torch.nn.CrossEntropyLoss()
 
-    writer = SummaryWriter(log_dir=os.path.join("logs", args.name))
-
-    paras = model.parameters()
-    base_optimizer = torch.optim.SGD(model.parameters(),lr=args.learning_rate,momentum=0.9,weight_decay=args.weight_decay)
-    optimizer = ESAM(paras, base_optimizer, rho=args.rho, beta=args.beta,gamma=args.gamma,adaptive=args.isASAM,nograd_cutoff=args.nograd_cutoff)
-    optimizer0 = optimizer.base_optimizer
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer0, T_max=args.epochs)
-
-    #half float setting 
-    if args.fp16:
-        model, [optimizer0,optimizer] = amp.initialize(models=model,
-                                      optimizers=[optimizer0,optimizer],
-                                      opt_level=args.fp16_opt_level)
-        amp._amp_state.loss_scalers[0]._loss_scale = 2**20
-        opt_list = [optimizer0,optimizer]
-
-    # Distributed training
-    if args.local_rank != -1:
-        model = DDP(model,device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=False)
-
-
-
-    best_acc = 0.0
-    global_step = -1
-    sampler = dataset.train.sampler
+    best_accuracy = 0.
     for epoch in range(args.epochs):
-        if args.local_rank != -1:
-            sampler.set_epoch(epoch)
+        # Train
         model.train()
-        log.train(len_dataset=len(dataset.train))
+        loss = 0.
+        accuracy = 0.
+        cnt = 0.
+        for inputs, targets in train_loader:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
 
-        for batch in dataset.train:
-            global_step += 1
-            # inputs, targets = (b.to(args.device) for b in batch)
-            inputs, targets = (b.to(args.device) for b in batch)
+            # Ascent Step
+            predictions = model(inputs)
+            batch_loss = criterion(predictions, targets)
+            batch_loss.mean().backward()
+            minimizer.ascent_step()
 
-            def defined_backward(loss):
-                if args.fp16:
-                    with amp.scale_loss(loss, optimizer0) as scaled_loss:
-                    # with amp.scale_loss(loss, [optimizer0,optimizer]) as scaled_loss:
-                        scaled_loss.backward()
-                else:
-                    loss.backward()
-
-            paras = [inputs,targets,loss_fct,model,defined_backward]
-            optimizer.paras = paras
-            optimizer.step()
-            predictions,loss = optimizer.returnthings
-
-
- 
+            # Descent Step
+            criterion(model(inputs), targets).mean().backward()
+            minimizer.descent_step()
 
             with torch.no_grad():
-                if len(inputs)!=len(predictions):
-                    continue
-                correct = torch.argmax(predictions.data, 1) == targets
-                log(model, loss.cpu(), correct.cpu(), scheduler.get_last_lr()[0])
-                acc = (correct.sum()+0.01) / (len(targets)+0.01) 
-
-            if  args.local_rank in [-1, 0]:
-                writer.add_scalar("train/loss", scalar_value=loss.mean(), global_step=global_step)
-                writer.add_scalar("train/acc", scalar_value=acc, global_step=global_step)
-
+                loss += batch_loss.sum().item()
+                accuracy += (torch.argmax(predictions, 1) == targets).sum().item()
+            cnt += len(targets)
+        loss /= cnt
+        accuracy *= 100. / cnt
+        print(f"Epoch: {epoch}, Train accuracy: {accuracy:6.2f} %, Train loss: {loss:8.5f}")
+        writer.add_scalar('Loss/train', scalar_value = loss, global_step = epoch)
+        writer.add_scalar('Accuracy/train', scalar_value= accuracy, global_step = epoch)
         scheduler.step()
-        if  args.local_rank in [-1, 0]:
-            model.eval()
-            log.eval(len_dataset=len(dataset.test))
 
-            with torch.no_grad():
-                tol_cor = 0
-                tol_len = 0
-                for batch in dataset.test:
-                    inputs, targets = (b.to(device) for b in batch)
-
-                    predictions = model(inputs)
-                    loss = smooth_crossentropy(predictions, targets)
-                    correct = torch.argmax(predictions, 1) == targets
-                    log(model, loss.cpu(), correct.cpu())
-                    acc = (correct.sum()+0.01) / (len(targets)+0.01) 
-                    tol_len += len(targets)
-                    tol_cor += correct.sum()
-                acc = tol_cor/(tol_len+0.0)
-                if acc > best_acc:
-                    best_acc = acc 
-                    model_to_save = model.module if hasattr(model, 'module') else model
-                    torch.save(model_to_save.state_dict(),"../output/"+"%s_checkpoint.bin" %args.name)
-                writer.add_scalar("test/acc", scalar_value=tol_cor/(tol_len+0.0), global_step=global_step)
-    if args.local_rank in [-1,0]:
-        email_text = "training of {} finished, best acc is {}".format(args.name,best_acc) 
-        send_email(email_text)
-        log.flush()
-
-def main(args):
-    # Setup CUDA, GPU & distributed training
-    if args.local_rank == -1:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        args.n_gpu = torch.cuda.device_count()
-    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-        torch.cuda.set_device(args.local_rank)
-        device = torch.device("cuda", args.local_rank)
-        torch.distributed.init_process_group(backend='nccl',
-                                             timeout=timedelta(minutes=60))
-        args.n_gpu = 1
-    args.device = device
-
-    # Setup logging
-    train_name = "train" 
-    log_path = args.name + "_" + train_name
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',filename = '../output/logs/'+log_path,
-                        datefmt='%m/%d/%Y %H:%M:%S',
-                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
-    logger.warning("Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s" %
-                   (args.local_rank, args.device, args.n_gpu, bool(args.local_rank != -1), args.fp16))
-
-    # Set seed
-
-    # Model & Tokenizer Setup
-    model = setup_model(args)
-
-
-    # Training
-    train(args, model)
+        # Test
+        model.eval()
+        loss = 0.
+        accuracy = 0.
+        cnt = 0.
+        with torch.no_grad():
+            for inputs, targets in test_loader:
+                inputs = inputs.cuda()
+                targets = targets.cuda()
+                predictions = model(inputs)
+                loss += criterion(predictions, targets).sum().item()
+                accuracy += (torch.argmax(predictions, 1) == targets).sum().item()
+                cnt += len(targets)
+            loss /= cnt
+            accuracy *= 100. / cnt
+        if best_accuracy < accuracy:
+           best_accuracy = accuracy
+        print(f"Epoch: {epoch}, Test accuracy:  {accuracy:6.2f} %, Test loss:  {loss:8.5f}")
+        writer.add_scalar('Loss/Test', scalar_value = loss)
+        writer.add_scalar('Accuracy/Test', scalar_value = accuracy)
+    print(f"Best test accuracy: {best_accuracy}")
 
 
 if __name__ == "__main__":
-    main(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default='CIFAR10', type=str, help="CIFAR10 or CIFAR100.")
+    parser.add_argument("--model", default='PyramidNet', type=str, help="Name of model architecure")
+    parser.add_argument("--minimizer", default='SAM', type=str, help="ASAM or SAM.")
+    parser.add_argument("--lr", default=0.1, type=float, help="Initial learning rate.")
+    parser.add_argument("--momentum", default=0.9, type=float, help="Momentum.")
+    parser.add_argument("--weight_decay", default=0, type=float, help="Weight decay factor.")
+    parser.add_argument("--batch_size", default=256, type=int, help="Batch size")
+    parser.add_argument("--epochs", default=300, type=int, help="Number of epochs.")
+    parser.add_argument("--smoothing", default=0.1, type=float, help="Label smoothing.")
+    parser.add_argument("--rho", default=0.2, type=float, help="Rho for ASAM.")
+    parser.add_argument("--eta", default=0.0, type=float, help="Eta for ASAM.")
+    args = parser.parse_args()
+    assert args.dataset in ['CIFAR10', 'CIFAR100'], \
+            f"Invalid data type. Please select CIFAR10 or CIFAR100"
+    assert args.minimizer in ['ASAM', 'SAM'], \
+            f"Invalid minimizer type. Please select ASAM or SAM"
+    train(args)
